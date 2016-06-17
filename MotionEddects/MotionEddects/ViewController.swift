8244c4f209a99ae90cd595938ef08264fe1a8a03
Heiyuan <Dayuan@liuzhiyuandeMacBook-Pro.local>
2016-06-17 18:35:58 +0800

    完成..

--- MotionEddects/MotionEddects/ViewController.swift
+++ MotionEddects/MotionEddects/ViewController.swift
@@ -17,14 +17,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         UIApplication.shared().setStatusBarHidden(true, with: UIStatusBarAnimation.none)
-        let mountainEddectX:UIInterpolatingMotionEffect = UIInterpolatingMotionEffect.init(keyPath: "center", type: UIInterpolatingMotionEffectType.tiltAlongHorizontalAxis)
+        let mountainEddectX:UIInterpolatingMotionEffect = UIInterpolatingMotionEffect.init(keyPath: "center.x", type: UIInterpolatingMotionEffectType.tiltAlongHorizontalAxis)
         mountainEddectX.maximumRelativeValue = 50.0
         mountainEddectX.minimumRelativeValue = -50.0
         
+        let mountainEddectY:UIInterpolatingMotionEffect = UIInterpolatingMotionEffect.init(keyPath: "center.y", type: UIInterpolatingMotionEffectType.tiltAlongVerticalAxis)
+        mountainEddectY.maximumRelativeValue = 50.0
+        mountainEddectY.minimumRelativeValue = -50.0
         self.catImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height))
         self.view.addSubview(self.catImageView!)
         self.catImageView?.image = UIImage(named: "head.jpg")
         self.catImageView!.addMotionEffect(mountainEddectX)
+        self.catImageView!.addMotionEffect(mountainEddectY);
         
         
     }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# MotionEddects
->运动效果
->iOS7 开始出现的手机桌面随着手机的转动,背景桌面的改变
+#iOS7之后桌面背景效果
+>iOS7开始桌面背景会根据[陀螺仪](http://baike.baidu.com/link?url=BugS06F8DFJO8S7xzybmoVguhu5E7gDzW0PiCKAm3RIJl36HfAOUXeE2qCJ0Vei1ns26zv4-AyQf5iEq1GdYJOZ7uA7arWbvpuzLPZZ2G-W)的效果改变晚上发现网上没有相关demol(可能是我笨没搜到).
+
+Demol在这:[Demol](https://github.com/Heiyuan/MotionEddects) 
